09c8f187296a2226dfb5f651f4629441de3bae76
PTrue81 <37983082+PTrue81@users.noreply.github.com>
2018-04-05 22:13:19 -0400

    Updated Apps

---
+++ RideShare/app/src/main/java/com/google/example/rideshare/SuccessfulRideSearch.java
@@ -0,0 +1,13 @@
+package com.google.example.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SuccessfulRideSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_ride_search);
+    }
+}
